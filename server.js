8130ee531e9b7991b2e7af7b069027fadd5559b2
Akmal-ui <akmalmalik6899@gmail.com>
2026-04-30 01:03:24 +0530

    allow all cors

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const adminAuthRoutes = require("./routes/adminAuth");
 
 // Middleware
 app.use(cors({
-  origin: "https://polite-field-0b92ff700.7.azurestaticapps.net",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  origin: "*"
 }));
 app.use(express.json());
 
